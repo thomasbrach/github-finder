f82a536213a15e72c43e9cc0a738325e1ae7a2a7
ironsocks <brach.thomasja@gmail.com>
2020-11-09 18:04:33 +0100

    final code

---
+++ src/components/users/Search.jsx
@@ -0,0 +1,50 @@
+import React, { useState, useContext } from "react";
+import GithubContext from "../../context/github/githubContext";
+import AlertContext from "../../context/alert/alertContext";
+
+const Search = () => {
+  const githubContext = useContext(GithubContext);
+  const alertContext = useContext(AlertContext);
+  const [searchbox, setSearchbox] = useState(null);
+
+  const handleChange = (event) => setSearchbox(event.target.value);
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    if (!searchbox) {
+      alertContext.setAlert("Please enter a username", "light");
+    } else {
+      githubContext.searchUsers(searchbox);
+      setSearchbox("");
+    }
+  };
+
+  return (
+    <div>
+      <form className="form" onSubmit={handleSubmit}>
+        <input
+          type="text"
+          name="text"
+          placeholder="Search users..."
+          value={searchbox}
+          onChange={handleChange}
+        />
+        <input
+          type="submit"
+          value="Search"
+          className="btn btn-dark btn-block"
+        />
+      </form>
+      {githubContext.users.length > 0 && (
+        <button
+          className="btn btn-light btn-block"
+          onClick={githubContext.clearUsers}
+        >
+          Clear
+        </button>
+      )}
+    </div>
+  );
+};
+
+export default Search;
